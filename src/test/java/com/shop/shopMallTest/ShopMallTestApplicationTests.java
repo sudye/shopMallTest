1889854bca270a6337df3c2bb5ac6bf985488031
sudye <simjaye@naver.com>
2022-12-16 16:31:12 +0900

    wdwa

---
+++ src/test/java/com/shop/shopMallTest/ShopMallTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shopMallTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMallTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
